4c28af80cd58f4454247b6334504d9c18b0040a6
Robert Markowitz <robm@robm.org>
2018-04-24 17:52:32 -0700

    Prettying up the code with comments and small changes

--- info.r
+++ info.r
@@ -3,8 +3,9 @@ library("dplyr")
 options(width=200)
 
 # unzip the data file
-unzip("data.zip")
+if (!file.exists("arrest_data.csv")) { unzip("data.zip") }
 # read the data in
+if (!file.exists("arrest_data.csv")) { stop("No Data File")}
 df <- read.table("arrest_data.csv", header=TRUE,sep=",",stringsAsFactors=FALSE, as.is=TRUE)
 # the columns our for testing
 #  [1] "Report.ID"                "Arrest.Date"             
@@ -23,18 +24,21 @@ dayLabs<-c("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday
 df$Days <- factor(df$Days, levels=dayLabs)
 genderLabs<-c("M","F")
 df$Sex.Code<- factor(df$Sex.Code,levels=genderLabs)
+
 # remove anything without a description
 df <- df[!(df$Charge.Group.Description== ""), ]
 # convert times into to hour buckets
 df$Time <- round(df$Time/100)*100
-
 # make this field more useful.....not sure what an infraction or dependent is yet.
 df$Arrest.Type.Code <- recode(df$Arrest.Type.Code , "I" =  "Infraction" , "D" = "Dependent" , "F" = "Felony" , "M" = "Misdemeanor" , "O" = "Other")
 # change codes into human reable races
 df$Descent.Code <- recode(df$Descent.Code, "A" = "Other Asian" , "B" = "Black", "C" = "Chinese", "D" = "Cambodian",  "F" = "Filipino", "G" =  "Guamanian", "H" = "Hispanic/Latin/Mexican", "I" = "American Indian/Alaskan Native",  "J" = "Japanese", "K" = "Korean", "L" = "Laotian", "O" = "Other", "P" = "Pacific Islander", "S" = "Samoan",  "U" = "Hawaiian", "V" = "Vietnamese", "W" = "White", "X" = "Unknown", "Z" = "Asian Indian")
+
 # do crosstab on gender
 CrossTable(x=df$Sex.Code, y=df$Days, dnn=c("Gender","Days"),prop.r=FALSE,prop.c=FALSE,prop.t=FALSE,prop.chisq=FALSE, max.width=120)
+# do another table on gender, arrests, and days
 y = xtabs(~ Arrest.Type.Code + Days + Sex.Code , df)
 print(y)
+# do another table but see how ethnicity plays ino the information
 y = xtabs(~ Descent.Code + Days + Arrest.Type.Code + Sex.Code , df)
 print(y)
